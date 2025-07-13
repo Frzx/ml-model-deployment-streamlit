f29df0e2c9117e36b90a1f1b9d20961d2aee1264
ahmad.faraz <ahmadfaraz196@gmail.com>
2025-07-14 00:16:54 +0530

    code release

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from transformers import pipeline
+import torch
+
+import os
+import boto3
+
+BUCKET_NAME = "mlops-kgptalkie-faraz" # global variable
+
+s3 = boto3.client('s3')
+def download_dir(local_dir,s3_prefix):
+    os.makedirs(local_dir,exist_ok=True)
+    paginator = s3.get_paginator('list_objects_v2')
+    for result in paginator.paginate(Bucket = BUCKET_NAME,Prefix=s3_prefix):
+        if 'Contents' in result:
+            for key in result['Contents']:
+                s3_key = key['Key']
+
+                local_file_path = os.path.join(local_dir,os.path.relpath(s3_key,s3_prefix))
+                s3.download_file(BUCKET_NAME,s3_key,local_file_path)
+
+
+
+    
+
+st.title("Sentiment Analysis")
+
+download_model_button = st.button("Download Model")
+
+if download_model_button:
+    with st.spinner("Donwloading.Please wait..."):
+        local_dir = 'tinybert-sentiment-analysis'
+        s3_prefix = "ml-models/tinybert-sentiment-analysis/"
+        download_dir(local_dir,s3_prefix)
+
+review = st.text_area("Enter your review","Type...")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+classifier = pipeline("text-classification","tinybert-sentiment-analysis",device=device)
+predict_button = st.button("Predict")
+if predict_button:
+    output = classifier.predict(review)
+    st.write(output)
